d62b3fe86a1551db6bde4ecbd057e237f6cf2b59
FelipeOsano1 <felipe@pineapple-lab.com>
2024-04-10 12:18:33 -0300

    add get _videoPlayerPlatform with init()

--- lib/cached_video_player.dart
+++ lib/cached_video_player.dart
@@ -14,10 +14,20 @@ export 'package:video_player_platform_interface/video_player_platform_interface.
 import 'src/closed_caption_file.dart';
 export 'src/closed_caption_file.dart';
 
-final VideoPlayerPlatform _videoPlayerPlatform = VideoPlayerPlatform.instance
-  // This will clear all open videos on the platform when a full restart is
-  // performed.
-  ..init();
+VideoPlayerPlatform? _lastVideoPlayerPlatform;
+// This will clear all open videos on the platform when a full restart is
+// performed.
+
+VideoPlayerPlatform get _videoPlayerPlatform {
+  final VideoPlayerPlatform currentInstance = VideoPlayerPlatform.instance;
+  if (_lastVideoPlayerPlatform != currentInstance) {
+    // This will clear all open videos on the platform when a full restart is
+    // performed.
+    currentInstance.init();
+    _lastVideoPlayerPlatform = currentInstance;
+  }
+  return currentInstance;
+}
 
 /// The duration, current position, buffering state, error state and settings
 /// of a [CachedVideoPlayerController].
